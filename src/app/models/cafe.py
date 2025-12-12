b956a4ca0b4b2942a7fee9f01a02ead4f4047099
PashaDyakonov <vegence14@gmail.com>
2025-12-12 19:39:20 +0300

    Add cafe model

---
+++ src/app/models/cafe.py
@@ -0,0 +1,62 @@
+from typing import TYPE_CHECKING
+from uuid import UUID
+
+from sqlalchemy import ForeignKey, Index, String, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.db.base import Base
+from app.core.constants import Limits
+
+if TYPE_CHECKING:
+    from app.api.v1.media.models import Media
+    from app.api.v1.tables.models import Table
+
+
+class Cafe(Base):
+    """Модель кафе/ресторана."""
+
+    __tablename__ = "cafes"
+    __table_args__ = (
+        Index("idx_cafe_active", "active"),
+        Index("idx_cafe_name", "name"),
+        Index("idx_cafe_address", "address"),
+        Index("idx_cafe_phone", "phone"),
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(
+        String(Limits.MAX_CAFE_NAME_LENGTH),
+        unique=True,
+        nullable=False
+    )
+    address: Mapped[str] = mapped_column(
+        String(Limits.MAX_DESCRIPTION_LENGTH),
+        nullable=False
+    )
+    phone: Mapped[str] = mapped_column(
+        String(Limits.MAX_PHONE_LENGTH),
+        nullable=False
+    )
+    description: Mapped[str | None] = mapped_column(
+        Text,
+        nullable=True
+    )
+    photo_id: Mapped[UUID | None] = mapped_column(
+        ForeignKey("media.id", ondelete="SET NULL"),
+        nullable=True,
+    )
+    active: Mapped[bool] = mapped_column(default=True)
+    tables: Mapped[list["Table"]] = relationship(
+        "Table",
+        back_populates="cafe",
+        cascade="all, delete-orphan",
+    )
+
+    photo: Mapped["Media | None"] = relationship(
+        "Media",
+        foreign_keys=[photo_id],
+        back_populates="cafe_photos",
+    )
+
+    def __repr__(self) -> str:
+        return f"<Cafe(id='{self.id}', name='{self.name}')>"

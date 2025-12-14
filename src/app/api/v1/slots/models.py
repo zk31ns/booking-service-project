a5e7424742a110ab87080a4de309b0de39600be0
aleon <azaretov123@gmail.com>
2025-12-15 01:23:50 +0300

    chore: add Slot migration

--- src/app/api/v1/slots/models.py
+++ src/app/api/v1/slots/models.py
@@ -3,14 +3,15 @@ from sqlalchemy import ForeignKey, Time, UniqueConstraint, CheckConstraint, Inde
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
-from src.app.db.base import Base
+from app.db.base import Base
 
 
 class Slot(Base):
     __tablename__ = 'slots'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    cafe_id: Mapped[int] = mapped_column(ForeignKey('cafes.id'), nullable=False)
+    # Нужно будет заменить на ForeignKey когда будет таблица Cafe
+    cafe_id: Mapped[int] = mapped_column(nullable=False)
     start_time: Mapped[time] = mapped_column(Time, nullable=False)
     end_time: Mapped[time] = mapped_column(Time, nullable=False)
     created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)

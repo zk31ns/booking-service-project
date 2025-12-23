4bb5dd6ef502412c1a17577a1b8be8db15dd3857
Alexander Davydov <zk31ns@mail.ru>
2025-12-23 16:58:49 +0300

    изменить импорт на абсолютный from src.app.db.session import async_session_maker

--- src/app/api/dependencies.py
+++ src/app/api/dependencies.py
@@ -2,7 +2,7 @@ from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.db.session import async_session_maker
+from src.app.db.session import async_session_maker
 
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:

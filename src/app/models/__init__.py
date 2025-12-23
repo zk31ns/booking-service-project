63f410bd54b275551a01b79b5adec98a145c8a3e
Anastasia Kruglova <nastya.krug083@mail.ru>
2025-12-23 18:44:39 +0300

    Fix style and delete Unique check in model

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -5,10 +5,18 @@ from src.app.models.booking import Booking, TableSlot
 from src.app.models.cafe import Cafe
 from src.app.models.dish import Dish
 from src.app.models.models import User, cafe_managers
+from src.app.models.slot import Slot
 from src.app.models.table import Table
 
 __all__ = [
-    'User', 'cafe_managers', 'Dish', 'Action', 'Cafe', 'Table',
-    'Booking', 'TableSlot'
+    'User',
+    'cafe_managers',
+    'Dish',
+    'Action',
+    'Cafe',
+    'Table',
+    'Booking',
+    'TableSlot',
+    'Slot',
 ]
-__models__ = [User, Cafe, Table, Dish, Action, Booking]
+__models__ = [User, Cafe, Table, Dish, Action, Booking, Slot]
--- src/app/models/booking.py
+++ src/app/models/booking.py
@@ -1,54 +1,66 @@
-from datetime import datetime
+from datetime import date, datetime
 
 from sqlalchemy import (
-    Date, ForeignKey, Index, Integer,
-    String, UniqueConstraint, func
+    Date,
+    ForeignKey,
+    Index,
+    Integer,
+    String,
+    func,
 )
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from src.app.db.base import Base
 from src.app.core.constants import BookingStatus, Limits
+from src.app.db.base import Base
 
 
 class TableSlot(Base):
     """Модель связи временных окошек и столиков."""
-    __tablename__ = "table_slots"
+
+    __tablename__ = 'table_slots'
     __table_args__ = (
-        UniqueConstraint("table_id", "slot_id", name="uq_table_slot"),
-        Index("ix_table_slot_booking", "booking_id"),
-        Index("ix_table_slot_table_slot", "table_id", "slot_id"),
+        Index('ix_table_slot_booking', 'booking_id'),
+        Index('ix_table_slot_table_slot', 'table_id', 'slot_id'),
     )
 
     id: Mapped[int] = mapped_column(primary_key=True)
     booking_id: Mapped[int] = mapped_column(
-        ForeignKey("bookings.id", ondelete="CASCADE"),
-        nullable=False
+        ForeignKey('bookings.id', ondelete='CASCADE'), nullable=False
     )
     table_id: Mapped[int] = mapped_column(
-        ForeignKey("tables.id"),
-        nullable=False
+        ForeignKey('tables.id'), nullable=False
     )
     slot_id: Mapped[int] = mapped_column(
-        ForeignKey("slots.id"),
-        nullable=False
+        ForeignKey('slots.id'), nullable=False
     )
 
-    table = relationship("Table")
-    slot = relationship("Slot")
-    booking = relationship("Booking", back_populates="table_slots")
+    table = relationship(
+        'Table',
+        lazy='selectin',
+    )
+    slot = relationship(
+        'Slot',
+        lazy='selectin',
+    )
+    booking = relationship(
+        'Booking',
+        back_populates='table_slots',
+        lazy='selectin',
+    )
 
 
 class Booking(Base):
     """Модель бронирований."""
+
     __tablename__ = 'bookings'
     __table_args__ = (
-        Index("ix_booking_cafe", "cafe_id"),
-        Index("ix_booking_user", "user_id"),
-        Index("ix_booking_date", "booking_date"),
+        Index('ix_booking_cafe', 'cafe_id'),
+        Index('ix_booking_user', 'user_id'),
+        Index('ix_booking_date', 'booking_date'),
     )
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    booking_date: Mapped[datetime] = mapped_column(
+    booking_date: Mapped[date] = mapped_column(
         Date,
         nullable=False,
     )
@@ -60,37 +72,26 @@ class Booking(Base):
     note: Mapped[str | None] = mapped_column(
         String(Limits.MAX_BOOKING_NOTE_LENGTH)
     )
-    guest_number: Mapped[int] = mapped_column(
-        Integer,
-        nullable=False
-    )
+    guest_number: Mapped[int] = mapped_column(Integer, nullable=False)
     table_slots: Mapped[list[TableSlot]] = relationship(
-        "TableSlot",
-        back_populates="booking",
-        lazy="selectin",
-        cascade="all, delete-orphan",
+        'TableSlot',
+        back_populates='booking',
+        lazy='selectin',
+        cascade='all, delete-orphan',
     )
-    is_active: Mapped[bool] = mapped_column(
-        nullable=False,
-        default=True
-    )
+    is_active: Mapped[bool] = mapped_column(nullable=False, default=True)
     created_at: Mapped[datetime] = mapped_column(
-        nullable=False,
-        default=func.now()
+        nullable=False, server_default=func.now()
     )
     updated_at: Mapped[datetime] = mapped_column(
-        nullable=False,
-        default=func.now(),
-        onupdate=func.now()
+        nullable=False, server_default=func.now(), server_onupdate=func.now()
     )
 
     user_id: Mapped[int] = mapped_column(
-        ForeignKey('users.id'),
-        nullable=False
+        ForeignKey('users.id'), nullable=False
     )
     cafe_id: Mapped[int] = mapped_column(
-        ForeignKey('cafes.id'),
-        nullable=False
+        ForeignKey('cafes.id'), nullable=False
     )
-    user = relationship('User', back_populates='bookings', lazy="selectin",)
-    cafe = relationship('Cafe', lazy="selectin",)
+    user = relationship('User', back_populates='bookings', lazy='selectin')
+    cafe = relationship('Cafe', lazy='selectin')

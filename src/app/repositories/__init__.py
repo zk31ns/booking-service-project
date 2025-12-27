7ed25977a4f03bc08b077573d3a83a973cce4935
Anastasia Kruglova <nastya.krug083@mail.ru>
2025-12-27 19:22:21 +0300

    add booking import

--- src/app/repositories/__init__.py
+++ src/app/repositories/__init__.py
@@ -1,7 +1,9 @@
 from .cafe import CafeRepository
 from .table import TableRepository
+from .booking import BookingRepository
 
 __all__ = [
     'CafeRepository',
     'TableRepository',
+    'BookingRepository'
 ]
